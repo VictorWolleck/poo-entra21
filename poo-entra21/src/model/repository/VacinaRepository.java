061a177a023c27b8499f75e4c0a8382cfc1d836c
Guest 01 <guest01@alunos.sc.senac.br>
2022-08-17 22:07:58 -0300

    fifth commit

--- poo-entra21/src/model/repository/VacinaRepository.java
+++ poo-entra21/src/model/repository/VacinaRepository.java
@@ -1,67 +1,156 @@
 package model.repository;
 
-import model.Banco;
-import model.entities.Vacina;
-
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import model.Banco;
+import model.entities.Vacina;
+
 public class VacinaRepository {
-	
-	//CRUD
-	//Create
+
 	public Vacina inserir(Vacina novaVacina) {
-		//Conectar no banco
-				Connection conexao = Banco.getConnection();
-				String sql = ;
+		Connection conexao = Banco.getConnection();
+
+		String sql = " INSERT INTO VACINA(PAIS_ORIGEM, ESTAGIO_PESQUISA, "
+				+ "                  DATA_INICIO_PESQUISA, NOME_RESPONSAVEL) " + " VALUES (?, ?, ?, ?) ";
+		PreparedStatement query = Banco.getPreparedStatementWithPk(conexao, sql);
+		try {
+			query.setString(1, novaVacina.getPais());
+			query.setInt(2, novaVacina.getEstagioDePesquisa());
+			query.setDate(3, new java.sql.Date(novaVacina.getInicioDaPesquisa().getTime()));
+			query.setString(4, novaVacina.getPesquisadorResponsavel());
+			query.execute();
+
+			ResultSet chavesGeradas = query.getGeneratedKeys();
+			if (chavesGeradas.next()) {
+				novaVacina.setId(chavesGeradas.getInt(1));
+			}
+		} catch (SQLException e) {
+			System.out.println("Erro ao inserir vacina.\nCausa: " + e.getCause());
+		} finally {
+			Banco.closePreparedStatement(query);
+			Banco.closeConnection(conexao);
+		}
+
 		return novaVacina;
 	}
-	
-	//Update
-	public Vacina atualizar(Vacina vacina) {
-		//TODO
-		//UPDATE VACINA SET ESTAGIO_PESQUISA = 2
-		//WHERE ID = 2;
-		return vacina;
+
+	public boolean atualizar(Vacina vacina) {
+		Connection conexao = Banco.getConnection();
+		boolean atualizou = false;
+
+		String sql = " UPDATE VACINA SET " + " PAIS_ORIGEM = ?, ESTAGIO_PESQUISA = ?, "
+				+ " DATA_INICIO_PESQUISA = ?, NOME_RESPONSAVEL = ? " + " WHERE ID = ? ";
+		PreparedStatement query = Banco.getPreparedStatement(conexao, sql);
+
+		try {
+			query.setString(1, vacina.getPais());
+			query.setInt(2, vacina.getEstagioDePesquisa());
+			query.setDate(3, new java.sql.Date(vacina.getInicioDaPesquisa().getTime()));
+			query.setString(4, vacina.getPesquisadorResponsavel());
+			query.setInt(5, vacina.getId());
+
+			int linhasAfetadas = query.executeUpdate();
+			atualizou = linhasAfetadas > 0;
+		} catch (SQLException e) {
+			System.out.println("Erro ao atualizar vacina.\nCausa: " + e.getCause());
+		} finally {
+			Banco.closePreparedStatement(query);
+			Banco.closeConnection(conexao);
+		}
+
+		return atualizou;
 	}
-	
-	//Delete
+
+	// Delete
 	public boolean excluir(int id) {
 		boolean excluiu = false;
-		
-		//Conectar no banco
+
+		// Conectar no banco
 		Connection conexao = Banco.getConnection();
-		String sql = " DELETE FROM VACINA "
-				+ " WHERE ID = ? ";
-		//Obter o preparedStatement
+		String sql = " DELETE FROM VACINA " + " WHERE ID = ? ";
+		// Obter o preparedStatement
 		PreparedStatement stmt = Banco.getPreparedStatement(conexao, sql);
 		try {
-			//Executar
+			// Executar
 			stmt.setInt(1, id);
 			int registrosExcluidos = stmt.executeUpdate();
 			excluiu = (registrosExcluidos > 0);
 		} catch (SQLException e) {
 			System.out.println("Erro ao excluir vacina.\nCausa: " + e.getCause());
+		} finally {
+			Banco.closePreparedStatement(stmt);
+			Banco.closeConnection(conexao);
 		}
-		
+
 		return excluiu;
 	}
-	
-	//Retrieve
+
+	// Retrieve
 	public Vacina pesquisarPorId(int id) {
-		//TODO
-		//SELECT * FROM VACINA
-		//WHERE ID = 2;
-		
+		Vacina vacinaBuscada = null;
+
+		Connection conexao = Banco.getConnection();
+		String sql = "SELECT * FROM VACINAS\r\n" + "		//WHERE ID = ?";
+
+		PreparedStatement stmt = Banco.getPreparedStatement(conexao, sql);
+
+		try {
+			stmt.setInt(1, id);
+			ResultSet resultado = stmt.executeQuery();
+
+			if (resultado.next()) {
+				vacinaBuscada = new Vacina();
+				vacinaBuscada.setId(resultado.getInt("id"));
+				vacinaBuscada.setEstagioDePesquisa(resultado.getInt("estagio_pesquisa"));
+				vacinaBuscada.setPesquisadorResponsavel(resultado.getString("nome_responsavel"));
+				vacinaBuscada.setPais(resultado.getString("pais_origem"));
+				vacinaBuscada.setInicioDaPesquisa(resultado.getDate("data_inicio_pesquisa"));
+
+			}
+		} catch (SQLException e) {
+			System.out.println("Erro ao buscar vacina com id = " + id + ".\nCausa" + e.getCause());
+		} finally {
+			Banco.closePreparedStatement(stmt);
+			Banco.closeConnection(conexao);
+		}
+
 		return null;
 	}
-	
-	public ArrayList<Vacina> pesquisarTodas(){
-		//TODO
-		//SELECT * FROM VACINA
+
+	public ArrayList<Vacina> pesquisarTodas() {
+
+		ArrayList<Vacina> vacinaRepository = new ArrayList<Vacina>();
+		Vacina vacinaBuscada = null;
+		Connection conexao = Banco.getConnection();
+		String sql = "SELECT * FROM VACINAS\r\n";
+
+		PreparedStatement stmt = Banco.getPreparedStatement(conexao, sql);
+
+		try {
+			ResultSet resultado = stmt.executeQuery();
+
+			while (resultado.next()) {
+				vacinaBuscada = new Vacina();
+				vacinaBuscada.setId(resultado.getInt("id"));
+				vacinaBuscada.setEstagioDePesquisa(resultado.getInt("estagio_pesquisa"));
+				vacinaBuscada.setPesquisadorResponsavel(resultado.getString("nome_responsavel"));
+				vacinaBuscada.setPais(resultado.getString("pais_origem"));
+				vacinaBuscada.setInicioDaPesquisa(resultado.getDate("data_inicio_pesquisa"));
+				vacinaRepository.add(vacinaBuscada);
+			}
+		} catch (SQLException e) {
+			System.out.println("Erro ao buscar vacinas =.\nCausa" + e.getCause());
+		} finally {
+			Banco.closePreparedStatement(stmt);
+			Banco.closeConnection(conexao);
+		}
+
 		return null;
 	}
-	
+
 }
--- poo-entra21/src/principal/Principal.java
+++ poo-entra21/src/principal/Principal.java
@@ -1,17 +1,13 @@
 package principal;
 
-import entidade.Endereco;
-import entidade.Funcionario;
+import model.repository.VacinaRepository;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		
-		Endereco endereco1 = new Endereco("12345", "Rua Roger Romeu", "45", "Prainha", "SC" );
-		Funcionario jose = new Funcionario("José Augustus", 2500, endereco1);
+		VacinaRepository teste = new VacinaRepository();
 		
-		
-		System.out.println(jose.toString());
-		
+		teste.pesquisarTodas();
 	}
 }
